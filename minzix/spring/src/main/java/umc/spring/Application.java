11dfebdd201e976f7a003f88da0c9a09f664f298
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-05-01 17:42:27 +0900

    [Feat(만두/김민지)] 홈 화면 쿼리에 QueryDSL 적용하기

--- minzix/spring/src/main/java/umc/spring/Application.java
+++ minzix/spring/src/main/java/umc/spring/Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import umc.spring.service.MissionService.MissionQueryService;
 import umc.spring.service.ReviewService.ReviewService;
 import umc.spring.service.StoreService.StoreQueryService;
 import umc.spring.service.MemberMissionService.MemberMissionService;
@@ -50,6 +51,9 @@ public class Application {
 			ReviewService reviewService = context.getBean(ReviewService.class);
 			reviewService.addReview(1L, 1L, 5, "분위기가 좋았어요!", "https://img.url/sample.jpg");
 
+			// MissionService 콘솔 로그 출력용
+			MissionQueryService missionQueryService = context.getBean(MissionQueryService.class);
+			missionQueryService.getMissionList("서울", null, 5);
 		};
 
 }}
