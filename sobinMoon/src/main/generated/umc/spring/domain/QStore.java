94f952565d3dadff89f65af4230741c01b9c1984
sobinMoon <sobin021206@naver.com>
2025-05-05 02:56:43 +0900

    [feat(비니/문소빈)] QueryDSL 적용 실습

--- sobinMoon/src/main/generated/umc/spring/domain/QStore.java
+++ sobinMoon/src/main/generated/umc/spring/domain/QStore.java
@@ -18,6 +18,8 @@ public class QStore extends EntityPathBase<Store> {
 
     private static final long serialVersionUID = 1158464437L;
 
+    private static final PathInits INITS = PathInits.DIRECT2;
+
     public static final QStore store = new QStore("store");
 
     public final umc.spring.domain.common.QBaseEntity _super = new umc.spring.domain.common.QBaseEntity(this);
@@ -33,6 +35,8 @@ public class QStore extends EntityPathBase<Store> {
 
     public final StringPath name = createString("name");
 
+    public final QRegion region;
+
     public final ListPath<Review, QReview> reviewList = this.<Review, QReview>createList("reviewList", Review.class, QReview.class, PathInits.DIRECT2);
 
     public final NumberPath<Float> score = createNumber("score", Float.class);
@@ -41,15 +45,24 @@ public class QStore extends EntityPathBase<Store> {
     public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
 
     public QStore(String variable) {
-        super(Store.class, forVariable(variable));
+        this(Store.class, forVariable(variable), INITS);
     }
 
     public QStore(Path<? extends Store> path) {
-        super(path.getType(), path.getMetadata());
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
     }
 
     public QStore(PathMetadata metadata) {
-        super(Store.class, metadata);
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QStore(PathMetadata metadata, PathInits inits) {
+        this(Store.class, metadata, inits);
+    }
+
+    public QStore(Class<? extends Store> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.region = inits.isInitialized("region") ? new QRegion(forProperty("region")) : null;
     }
 
 }
--- sobinMoon/src/main/java/umc/spring/domain/Store.java
+++ sobinMoon/src/main/java/umc/spring/domain/Store.java
@@ -31,4 +31,18 @@ public class Store extends BaseEntity {
 
     @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
     private List<Mission> missionList = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    public String toString() {
+        return "Store{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", score=" + score +
+                ", region=" + (region != null ? region.getName() : "N/A") + // region의 이름 출력
+                '}';
+    }
 }
