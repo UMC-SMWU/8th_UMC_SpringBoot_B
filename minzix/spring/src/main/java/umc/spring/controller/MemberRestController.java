6e1437395b13894f12b069515f453d14e2e19826
김민지 <minjikim@gimminjiui-MacBookAir.local>
2025-05-14 19:56:51 +0900

    [Fix(만두/김민지)] .gitignore 파일 업데이트 및 git cache 삭제

---
+++ minzix/spring/src/main/java/umc/spring/controller/MemberRestController.java
@@ -0,0 +1,25 @@
+package umc.spring.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.dto.Member.MemberRequestDTO;
+import umc.spring.dto.Member.MemberResponseDTO;
+import umc.spring.service.MemberCommandService.MemberCommandService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
+        return null;
+    }
+}
