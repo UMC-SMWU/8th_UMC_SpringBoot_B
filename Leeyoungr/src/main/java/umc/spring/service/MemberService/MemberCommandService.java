2d12adefd2c8df203e4f750dd403bcd98b0aafac
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2025-05-20 15:03:57 +0900

    [Feat(이영/이은영)] 8주차 워크북 실습

---
+++ Leeyoungr/src/main/java/umc/spring/service/MemberService/MemberCommandService.java
@@ -0,0 +1,8 @@
+package umc.spring.service.MemberService;
+
+import umc.spring.domain.Member;
+import umc.spring.web.dto.Request.MemberRequestDTO;
+
+public interface MemberCommandService {
+    Member joinMember(MemberRequestDTO.JoinDto request);
+}
