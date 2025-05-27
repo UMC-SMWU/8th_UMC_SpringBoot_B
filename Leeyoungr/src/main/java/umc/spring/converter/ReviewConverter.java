a5cd0c180799f5db504fe6e40efe58d3803c3262
2is0 <130418736+Leeyoungr@users.noreply.github.com>
2025-05-27 16:10:34 +0900

    [Feat(이영/이은영)] 매장 리뷰목록 조회 실습 API 구현

--- Leeyoungr/src/main/java/umc/spring/converter/ReviewConverter.java
+++ Leeyoungr/src/main/java/umc/spring/converter/ReviewConverter.java
@@ -1,11 +1,15 @@
 package umc.spring.converter;
 
+import org.springframework.data.domain.Page;
 import umc.spring.domain.Review;
 import umc.spring.domain.Store;
 import umc.spring.web.dto.Request.MemberRequestDTO;
 import umc.spring.web.dto.Response.MemberResponseDTO;
+import umc.spring.web.dto.Response.StoreResponseDTO;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReviewConverter {
     public static MemberResponseDTO.CreateReviewResponseDTO toCreateReivewResponseDTO(Review review){
@@ -20,6 +24,29 @@ public class ReviewConverter {
                 .store(store)
                 .body(request.getBody())
                 .score(request.getScore())
+                .build();
+    }
+
+    public static StoreResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
+        return StoreResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getMember().getName())
+                .score(review.getScore())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getBody())
+                .build();
+    }
+    public static StoreResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+
+        List<StoreResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(ReviewConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return StoreResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
                 .build();
     }
 }
