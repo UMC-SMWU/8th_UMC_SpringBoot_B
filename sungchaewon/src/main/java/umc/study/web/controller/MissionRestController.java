0b4f63b878f73b91d12581a588f0bc51636680f1
sungchaewon <scw226@sookmyung.ac.kr>
2025-05-29 17:34:08 +0900

    [fix(워니/성채원)] 가게의 미션을 도전 중인 미션에 추가(미션 도전하기) API 완성(controller 추가 및 수정)

--- sungchaewon/src/main/java/umc/study/web/controller/MissionRestController.java
+++ sungchaewon/src/main/java/umc/study/web/controller/MissionRestController.java
@@ -1,11 +1,8 @@
 package umc.study.web.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import umc.study.apiPayload.ApiResponse;
 import umc.study.service.MissionService.MissionChallengeService;
-import umc.study.web.dto.MissionChallengeRequestDTO;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,10 +11,5 @@ public class MissionRestController {
 
     private final MissionChallengeService missionChallengeService;
 
-    @PostMapping("/challenge")
-    public ApiResponse<String> challengeMission(@RequestBody @Valid MissionChallengeRequestDTO request) {
-        Long hardcodedMemberId = 1L; // 로그인 없으므로 하드코딩
-        missionChallengeService.challengeMission(hardcodedMemberId, request.getMissionId());
-        return ApiResponse.onSuccess("미션 도전이 완료되었습니다.");
-    }
 }
+
